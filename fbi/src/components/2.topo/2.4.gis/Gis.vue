02e7e6c7016dcb5529bcecf0b86245477e857b3c
JHercules <JHercules_qz@qq.com>
2017-10-31 09:42:02 +0800

    update gis

--- fbi/src/components/2.topo/2.4.gis/Gis.vue
+++ fbi/src/components/2.topo/2.4.gis/Gis.vue
@@ -83,7 +83,14 @@
       //region gisTopo
       _initGisTopo: function () {
         this.$refs.gisTopoPanel.initGisTopo(51.505, -0.09, 13, {
-          hasZoomCtrl: true
+          mapUrl: "https://api.mapbox.com/styles/v1/jherculesqz/cj99csbwi2bzy2qp3mhtqcrkx/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamhlcmN1bGVzcXoiLCJhIjoiY2o4anNybjZqMDZnczMybXZxaHNhMDRlMyJ9.fR2DM7aypSp8q4AxE6uL5w",
+          //mapUrl: "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={token}",
+          hasZoomCtrl: true,
+          export: true,
+          mess: true,
+          messTmpLine: "#fff",
+          messResLine: "#fff",
+          pm: true,
         });
       },
       _drawGisTopo: function () {
